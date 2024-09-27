863fafefcf0f39c41cb228dd8cee72da98e6bea7
André Perotto <138815928+andreperotto@users.noreply.github.com>
2024-09-27 16:27:06 -0300

    Update header_IA.tsx

--- sections/header_IA.tsx
+++ sections/header_IA.tsx
@@ -64,8 +64,8 @@ function Header({ logo, navItems, cta }: Props) {
               <Image
                 src={logo.image || ""}
                 alt={logo.alt || ""}
-                height={logo.height || }
-                width={logo.width || }
+                height={logo.height || 80 }
+                width={logo.width || 200 }
               />
             )}
           </a>
