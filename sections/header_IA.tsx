98d3af72cbf161db37590e38bf5d358a1ea519e4
André Perotto <andre@leadfy.me>
2024-09-28 18:42:47 +0000

    Publish 2 from staging into origin/main

--- sections/header_IA.tsx
+++ sections/header_IA.tsx
@@ -9,22 +9,27 @@ interface Props {
    * @description Alternative text for the logo
    */
   logoAlt?: string;
+  /**
+   * @description Background color of the header
+   * @format color-input
+   */
+  backgroundColor?: string;
 }
 
 export default function Header({
   logo = "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1818/ff6bb37e-0eab-40e1-a454-86856efc278e",
-  logoAlt = "Company Logo"
+  logoAlt = "Company Logo",
+  backgroundColor = "#ffffff"
 }: Props) {
   return (
-    <header class="bg-base-100 p-4">
-      <div class="container mx-auto px-4">
-        <div class="flex justify-start">
-          <img
-            src={logo}
-            alt={logoAlt}
-            class="w-[100px] h-[200px] object-contain"
-          />
-        </div>
+    <header class="p-4" style={{ backgroundColor, height: "30px" }}>
+      <div class="container mx-auto px-4 h-full flex items-center">
+        <img
+          src={logo}
+          alt={logoAlt}
+          class="w-[100px] h-[200px] object-contain"
+          style={{ marginTop: "100px" }}
+        />
       </div>
     </header>
   );
