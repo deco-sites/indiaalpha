cf3c84c487fccee9a29857df7ae15c40bc693fb6
André Perotto <andre@leadfy.me>
2024-09-28 18:37:06 +0000

    Publish 2 from staging into origin/main

--- sections/header_IA.tsx
+++ sections/header_IA.tsx
@@ -1,141 +1,34 @@
-
-import { useScript } from "deco/hooks/useScript.ts";
 import { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
-import Icon from "site/components/ui/Icon.tsx";
-
-interface Logo {
-  image?: ImageWidget;
-  alt?: string;
-  width?: number;
-  height?: number;
-}
-
-interface CTA {
-  href?: string;
-  text?: string;
-  /** @description Abrir o link em uma nova guia */
-  openInNewTab?: boolean;
-}
-
-interface NavItem {
-  label?: string;
-  href?: string;
-  /** @description Abrir o link em uma nova guia */
-  openInNewTab?: boolean;
-}
 
 interface Props {
-  logo?: Logo;
-  navItems?: NavItem[];
-  cta?: CTA;
+  /**
+   * @description Logo image
+   */
+  logo?: ImageWidget;
+  /**
+   * @description Alternative text for the logo
+   */
+  logoAlt?: string;
 }
 
-const script = () => {
-  // Make the header sticky on scroll
-  const header = document.getElementById("header");
-  if (header) {
-    globalThis.onscroll = function () {
-      if (globalThis.scrollY > 0) {
-        const headerHeight = header.offsetHeight;
-
-        header.classList.add("fixed");
-        header.style.boxShadow = "0 0 36px rgba(0, 0, 0, 0.5)";
-        document.body.style.paddingTop = `${headerHeight}px`;
-      } else {
-        header.classList.remove("fixed");
-        document.body.style.paddingTop = "0";
-        header.style.boxShadow = "none";
-      }
-    };
-  }
-};
-
-function Header({ logo, navItems, cta }: Props) {
+export default function Header({
+  logo = "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1818/ff6bb37e-0eab-40e1-a454-86856efc278e",
+  logoAlt = "Company Logo"
+}: Props) {
   return (
-    <>
-      <header id="header" class="top-0 left-0 w-full bg-base-100 z-50">
-        <div class="w-full mx-auto px-4 max-w-[1140px] py-4 flex items-center justify-between">
-          <a
-            href="/"
-            class="flex flex-row gap-1 items-center justify-center text-xs"
-          >
-            {logo && (
-              <Image
-                src={logo.image || ""}
-                alt={logo.alt || ""}
-                height={logo.height || 50 }
-                width={logo.width || 150 }
-              />
-            )}
-          </a>
-
-          <div class="w-full flex justify-end items-center gap-10">
-            {navItems && (
-              <>
-                <nav class="hidden md:flex flex-row gap-4 items-center justify-center text-base font-semibold">
-                  {navItems.map((item) => (
-                    <a
-                      href={item.href || ""}
-                      target={item.openInNewTab ? "_blank" : "_self"}
-                      class="hover:text-primary transition-colors duration-300"
-                    >
-                      {item.label}
-                    </a>
-                  ))}
-                </nav>
-                <div className="dropdown dropdown-end md:hidden">
-                  <div
-                    tabIndex={0}
-                    role="button"
-                    className="bg-primary text-primary-content flex items-center justify-center w-8 h-8"
-                  >
-                    <Icon
-                      class="shrink-0"
-                      id="Bars3"
-                      width={24}
-                      height={24}
-                      strokeWidth={1}
-                    />
-                  </div>
-                  <ul
-                    tabIndex={0}
-                    className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
-                  >
-                    {navItems.map((item) => (
-                      <li>
-                        <a
-                          target={item.openInNewTab ? "_blank" : "_self"}
-                          href={item.href || ""}
-                          class="hover:text-primary"
-                        >
-                          {item.label}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </>
-            )}
-
-            {cta && (
-              <a
-                href={cta.href || ""}
-                target={cta.openInNewTab ? "_blank" : "_self"}
-                class="hidden md:inline-flex btn btn-sm btn-primary h-10 text-base font-medium"
-              >
-                {cta.text}
-              </a>
-            )}
-          </div>
+    <header className="bg-base-100 p-4">
+      <div className="container mx-auto">
+        <div className="flex justify-center">
+          <Image
+            src={logo}
+            alt={logoAlt}
+            width={100}
+            height={200}
+            class="object-contain"
+          />
         </div>
-      </header>
-      <script
-        type="module"
-        dangerouslySetInnerHTML={{ __html: useScript(script) }}
-      />
-    </>
+      </div>
+    </header>
   );
 }
-
-export default Header;
